bee9b26f40b8ceb076e5e211150e8e223aadeb6b
Yoo-jin Ha <dbwls9649@gmail.com>
2021-07-05 22:43:54 +0900

    commit

--- kaggle_code/ic_kaggle.py
+++ kaggle_code/ic_kaggle.py
@@ -8,3 +8,18 @@ import pandas as pd
 import numpy as np
 
 import IPython
+
+# Data
+data_dir='./flickr30k_images/flickr30k_images'
+image_dir=f'{data_dir}/flickr30k_images'
+csv_file=f'{data_dir}/results.csv'
+
+df=pd.read_csv(csv_file, delimiter='|')
+
+print(f'[INFO] The shape of dataframe: {df.shape}')
+print(f'[INFO] The columns in the dataframe: {df.columns}')
+print(f'[INFO] Unique image names: {len(pd.unique(df["image_name"]))}')
+
+df.head(5)
+
+df.colums=['image_name']
